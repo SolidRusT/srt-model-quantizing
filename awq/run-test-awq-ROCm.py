87dd7d95106b3cf0d058295246145c6ed3f3f28c
Shaun Prince <suparious@solidrust.net>
2024-04-21 02:32:51 -0700

    session with MaxPower and Pablo-ce

--- awq/run-test-awq-ROCm.py
+++ awq/run-test-awq-ROCm.py
@@ -1,12 +1,12 @@
 from awq import AutoAWQForCausalLM
 from transformers import AutoTokenizer, TextStreamer
 
-#quant_path = "TheBloke/Mixtral-8x7B-Instruct-v0.1-AWQ"
-quant_path = "kaist-ai/mistral-orpo-capybara-7k"
+#model_path = "TheBloke/Mixtral-8x7B-Instruct-v0.1-AWQ"
+model_path = "kaist-ai/mistral-orpo-capybara-7k"
 
 # Load model
-model = AutoAWQForCausalLM.from_quantized(quant_path, fuse_layers=True)
-tokenizer = AutoTokenizer.from_pretrained(quant_path, trust_remote_code=True)
+model = AutoAWQForCausalLM.from_quantized(model_path, fuse_layers=True)
+tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
 streamer = TextStreamer(tokenizer, skip_prompt=True, skip_special_tokens=True)
 
 # Convert prompt to tokens
---
+++ rocm-recipe.sh
@@ -0,0 +1,10 @@
+## Ubuntu 22.04 minimal install
+sudo apt install python3.11-full
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.11 1
+#sudo update-alternatives --config python3
+#sudo update-alternatives --set python /usr/bin/python3.11
+python -m venv venv-rocm-3.11
+source ~/venv-rocm-3.11/bin/activate
+pip install --upgrade wheel wandb peft transformers datasets trl bitsandbytes accelerate
+# /home/shaun/.cache/huggingface/accelerate/default_config.yaml
+accelerate config
