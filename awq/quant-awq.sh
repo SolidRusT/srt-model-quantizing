ce6e229858c223ead1cf7aaa0243760741d2fb9a
Shaun Prince <suparious@solidrust.net>
2024-04-25 03:11:52 -0700

    refactor to be more dynamic

--- awq/quant-awq.sh
+++ awq/quant-awq.sh
@@ -4,67 +4,70 @@ export MODEL=$2
 
 export QUANTER="solidrust"
 export QUANT_SCRIPT="run-quant-awq.py"
+export APP_HOME=${APP_HOME:-"/opt/quant-awq"}
+export SRT_REPO="${APP_HOME}/repos/srt-model-quantizing/awq"
+export SRT_DATA="${APP_HOME}/data"
+
+mkdir -p ${APP_HOME}
 
 # Uncomment for single GPU
-export CUDA_VISIBLE_DEVICES=0
+#export CUDA_VISIBLE_DEVICES=0
 
 # Ensure that script stops on first error
 set -e
 
 function garbage_collect() {
   rm -rf ${HOME}/.cache/huggingface/hub/models--*
-  rm -rf *-AWQ
+  rm -rf ${SRT_DATA}/*-AWQ
 }
 
-function create_quant_repo() {
-    (huggingface-cli repo create --organization ${QUANTER} ${MODEL}-AWQ -y)
+function git_commit() {
+  wherami=$(pwd)
+  cd ${SRT_DATA}/${MODEL}-AWQ
+  git lfs install
+  git add .
+  git commit -m "$1"
+  git pull
+  git push
+  cd $wherami
 }
 
 function update_readme() {
-    sed -i "s/{AUTHOR}/${AUTHOR}/g" README.md
-    sed -i "s/{MODEL}/${MODEL}/g" README.md
+  sed -i "s/{AUTHOR}/${AUTHOR}/g" ${SRT_DATA}/${MODEL}-AWQ/README.md
+  sed -i "s/{MODEL}/${MODEL}/g" ${SRT_DATA}/${MODEL}-AWQ/README.md
+}
+
+function create_quant_repo() {
+  (huggingface-cli repo create --organization ${QUANTER} ${MODEL}-AWQ -y)
+}
+
+function processing_notice() {
+  cp ${SRT_REPO}/processing-notice.txt ${SRT_DATA}/${MODEL}-AWQ/README.md
+  update_readme
+  git_commit "add processing notice"
+}
+
+function add_quant_config() {
+  cp ${SRT_REPO}/quant_config.json ${SRT_DATA}/${MODEL}-AWQ/quant_config.json
+  git_commit "adding quant config"
 }
 
 function clone_quant_repo() {
-    garbage_collect
-    create_quant_repo
-    git lfs install
-    git clone "git@hf.co:${QUANTER}/${MODEL}-AWQ"
-    cd "${MODEL}-AWQ/"
-    cp ../processing-notice.txt README.md
-    update_readme
-    git add .
-    git commit -m "add processing notice"
-    git pull
-    git push
-    cd ..
+  git clone "git@hf.co:${QUANTER}/${MODEL}-AWQ" ${SRT_DATA}/${MODEL}-AWQ
 }
 
 function quant_model() {
-    python ${QUANT_SCRIPT} \
+  python ${SRT_REPO}/${QUANT_SCRIPT} \
     --model_path ${AUTHOR}/${MODEL} \
-    --quant_path ${MODEL}-AWQ \
+    --quant_path ${SRT_DATA}/${MODEL}-AWQ \
     --zero_point True --q_group_size 128 --w_bit 4 --version GEMM
-}
-
-function upload_quant() {
-    cd "${MODEL}-AWQ/"
-    git lfs install
-    git add .
-    git commit -m "adding AWQ model"
-    cp ../quant_config.json .
-    git add .
-    git commit -m "adding quant config"
-    cp ../initial-readme.txt README.md
-    update_readme
-    git add .
-    git commit -m "adding initial model card"
-    git pull
-    git push
-    cd ..
+  git_commit "adding AWQ model"
 }
 
 # Main Program
+garbage_collect
+create_quant_repo
 clone_quant_repo
+processing_notice
 quant_model
-upload_quant
+add_quant_config
