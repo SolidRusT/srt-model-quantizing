dd65f30df809b3e30e729f2e0c50fb46f08baf19
Shaun Prince <suparious@solidrust.net>
2024-05-11 14:51:35 -0700

    Code review of quant-awq.sh

--- awq/quant-awq.sh
+++ awq/quant-awq.sh
@@ -11,9 +11,9 @@ export SRT_DATA="${APP_HOME}/data"
 mkdir -p ${APP_HOME}
 
 # Uncomment for single GPU
-#export CUDA_VISIBLE_DEVICES=0
+# export CUDA_VISIBLE_DEVICES=0
 
-# Ensure that script stops on first error
+# Ensure that the script stops on the first error
 set -e
 
 function logger() {
@@ -27,14 +27,13 @@ function garbage_collect() {
 }
 
 function upload() {
-  message=${1:-"empty message"}
+  local message=${1:-"empty message"}
   logger "Uploading: ${message}"
   huggingface-cli upload "${QUANTER}/${MODEL}-AWQ" "${SRT_DATA}/${MODEL}-AWQ/" . --commit-message "${message}"
 }
 
 function update_readme() {
   logger "Spiff up the README"
-  # TODO: huggingface-cli upload username/my-space --remote https://example.com/data.csv
   sed -i "s/{AUTHOR}/${AUTHOR}/g" ${SRT_DATA}/${MODEL}-AWQ/README.md
   sed -i "s/{MODEL}/${MODEL}/g" ${SRT_DATA}/${MODEL}-AWQ/README.md
 }
@@ -45,27 +44,27 @@ function create_quant_repo() {
 }
 
 function processing_notice() {
-  logger "add processing notice"
+  logger "Add processing notice"
   cp ${SRT_REPO}/processing-notice.txt ${SRT_DATA}/${MODEL}-AWQ/README.md
   update_readme
-  upload "add processing notice"
+  upload "Add processing notice"
 }
 
 function add_quant_config() {
-  logger "add quant config"
+  logger "Add quant config"
   cp ${SRT_REPO}/quant_config.json ${SRT_DATA}/${MODEL}-AWQ/quant_config.json
-  upload "adding quant config"
+  upload "Adding quant config"
 }
 
 function add_model_card() {
-  logger "add model card"
+  logger "Add model card"
   cp ${SRT_REPO}/initial-readme.txt ${SRT_DATA}/${MODEL}-AWQ/README.md
   update_readme
-  upload "add default model card"
+  upload "Add default model card"
 }
 
 function clone_quant_repo() {
-  logger "add quant repo"
+  logger "Add quant repo"
   huggingface-cli download "${QUANTER}/${MODEL}-AWQ" --local-dir "${SRT_DATA}/${MODEL}-AWQ/"
   logger "$(ls -al ${SRT_DATA}/${MODEL}-AWQ)"
 }
@@ -78,42 +77,39 @@ function download_model() {
 function pytorch_check() {
   logger "Checking for pytorch binaries"
   
-  # Get the latest snapshot directory
   local snapshot_dir
   snapshot_dir=$(ls -1d ${HOME}/.cache/huggingface/hub/models--${AUTHOR}--${MODEL}/snapshots/* | tail -n 1)
-  
-  # Check for pytorch .bin files
+
   local pytorch_bins
   pytorch_bins=$(find "${snapshot_dir}" -name "pytorch_model-0*.bin")
 
-  # Convert pytorch .bin files to safetensors if they exist
   if [[ -n "${pytorch_bins}" ]]; then
     local convert_script="${APP_HOME}/repos/srt-model-quantizing/convert-pytorch-simple.py"
     local bin
 
     for bin in ${pytorch_bins}; do
       if ! python "${convert_script}" "${bin}"; then
-        logger "Error: Conversion failed for %s\n" "${bin}"
+        logger "Error: Conversion failed for ${bin}"
         return 1
       fi
     done
 
-    printf "Conversion completed successfully.\n"
+    logger "Conversion completed successfully."
   else
-    printf "No pytorch .bin files found for author '%s' and model '%s'.\n" "${AUTHOR}" "${MODEL}"
+    logger "No pytorch .bin files found for author '${AUTHOR}' and model '${MODEL}'."
   fi
 }
 
 function quant_model() {
   download_model
   pytorch_check
-  logger "quantize the model"
+  logger "Quantize the model"
   python ${SRT_REPO}/${QUANT_SCRIPT} \
     --model_path ${AUTHOR}/${MODEL} \
     --quant_path ${SRT_DATA}/${MODEL}-AWQ \
     --zero_point True --q_group_size 128 --w_bit 4 --version GEMM
   logger "$(ls -al ${SRT_DATA}/${MODEL}-AWQ)"
-  upload "adding AWQ model"
+  upload "Adding AWQ model"
 }
 
 # Main Program
